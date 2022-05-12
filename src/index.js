037fab7a1868931749c66f91167c813263959d94
jairo.evaristo <jairo.evaristo.externo@caixavidaeprevidencia.com.br>
2022-05-12 14:29:08 -0300

    add area logged out user

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
 
-const routes = require("./router");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,16 @@ const { Router } = require("express");
 
 const routes = Router();
 
+routes.get("/", (req, res) => {
+  return res.render("loja-view");
+});
+
 routes.get("/signin", (req, res) => {
   return res.render("login");
+});
+
+routes.get("/signup", (req, res) => {
+  return res.render("signup");
 });
 
 module.exports = routes;
